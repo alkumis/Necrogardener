c939a8814c57619d48e566a71d7711f1b9c87815
Shariq Rafeek <shariqrafeek@Shariqs-MacBook-Pro.local>
2022-01-29 14:16:35 +0530

    First remote commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    private Vector2 movementVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(movementVector.x, movementVector.y, 0));
+    }
+
+    public void UpdatedMoveVector(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        movementVector = inputVector * movementSpeed * Time.fixedDeltaTime;
+    }
+}
